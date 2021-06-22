6d5a20ec95c88a55365b57c4c84e4bd971f63bca
jobailla <jbaillais77@gmail.com>
2021-06-22 11:57:08 +0200

    update install

--- install.sh
+++ install.sh
@@ -3,25 +3,16 @@ echo -e "\e[33mSystem Update... \e[0m"
 sudo apt-get update -y
 echo -e "\e[33mInstalling Dependencies... \e[0m"
 sudo apt-get install -y git vim python-picamera python-yaml python3-pip
-sudo apt-get install -y proftpd
-sudo apt-get install -y imagemagick
-sudo apt-get install -y rclone
+read -p "Do you want install optionials dependencies ? (proftpd, imagemagick, rclone) ? (y/n) " yn
+while true; do
+	case $yn in
+		[Yy]* ) echo -e "\e[33mInstalling Optionnals Dependencies... \e[0m" && sudo apt-get install -y proftpd imagemagick rclone; break;;
+		[Nn]* ) exit;;
+        	* ) echo "Please answer yes or no.";;
+    	esac
+done
+echo -e "\e[33mInstalling Python Dependencies... \e[0m"
 pip3 install picamera Werkzeug PyYAML 
-mkdir /home/pi/Pictures
-# Witty Pi
-sudo su
-echo "dtoverlay=w1-gpio,gpiopin=18" >> /boot/config.txt
-exit
-cd /home/pi
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-sudo dpkg -i wiringpi-latest.deb
-wget http://www.uugear.com/repo/WittyPi2/installWittyPi.sh
-sudo sh installWittyPi.sh
-rm wiringpi-latest.deb
-mv installWittyPi.sh wittyPi
-echo "alias timelapse=\"python3 /home/pi/RPIZ-timelapse/timelapse.py\"" >> .bashrc
-echo "alias witty=\"sudo /home/pi/wittyPi/wittyPi.sh\"" >> .bashrc
-echo "alias test=\"python3 /home/pi/RPIZ-timelapse/test_camera.py\"" >> .bashrc
 # activate camera
 echo -e "\e[33mActivating Camera... (raspi-config)\e[0m"
 raspi-config nonint do_camera 0
@@ -32,14 +23,50 @@ if ! (grep -R "/usr/bin/tvservice -o" /home/pi/.zshrc 2>/dev/null)
 then
     echo -e "\n/usr/bin/tvservice -o" >> /home/pi/.zshrc
 fi
+if [ ! -d "/home/pi/Pictures" ]
+	echo -e "\e[33mCreate Pictures directory...\e[0m"
+then
+	mkdir /home/pi/Pictures
+fi
 # set crontab
 echo -e "\e[33mAdd crontab... \e[0m"
 mkdir /home/pi/logs
 ( (crontab -l -u pi 2>/dev/null || echo "") ; echo "@reboot sudo sh /home/pi/RPIZ-timelapse/scripts/start.sh > /home/pi/logs/log.txt 2>&1") | sort - | uniq - | crontab - -u pi
-# ask for reboot
-read -p "Reboot Now (needed to restart wifi settings) ? (y/n) " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-    echo -e "\n"
+# Witty Pi
+if [ ! -d "/home/pi/wittyPi" ]
 then
-    reboot
+	echo -e "\e[33mWitty Pi setup...\e[0m"
+	sudo echo "dtoverlay=w1-gpio,gpiopin=18" >> /boot/config.txt
+	cd /home/pi
+	wget https://project-downloads.drogon.net/wiringpi-latest.deb
+	sudo dpkg -i wiringpi-latest.deb
+	wget http://www.uugear.com/repo/WittyPi2/installWittyPi.sh
+	sudo sh installWittyPi.sh
+	rm wiringpi-latest.deb
+	mv installWittyPi.sh wittyPi
+	echo -e "\e[33mSet alias...\e[0m"
+	echo "alias timelapse=\"sudo python3 /home/pi/RPIZ-timelapse/timelapse.py\"" >> .bashrc
+	echo "alias witty=\"sudo /home/pi/wittyPi/wittyPi.sh\"" >> .bashrc
+	echo "alias test=\"sudo python3 /home/pi/RPIZ-timelapse/test_camera.py\"" >> .bashrc
 fi
+# Timezone
+echo "current timezone:"
+sudo timedatectl | grep "Time zone:"
+read -p "Do you want to change the timezone ? (raspi-config) ? (y/n) " yn
+while true; do
+	case $yn in
+		[Yy]* ) sudo raspi-config; break;;
+		[Nn]* ) exit;;
+        	* ) echo "Please answer yes or no.";;
+    	esac
+done
+# ask for reboot
+
+read -p "Reboot Now (needed to restart wifi settings) ? (y/n) " yn
+while true; do
+	case $yn in
+		[Yy]* ) sudo raspi-config; break;;
+		[Nn]* ) exit;;
+        	* ) echo "Please answer yes or no.";;
+    	esac
+done
