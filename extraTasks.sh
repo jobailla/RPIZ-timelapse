0fab0007167584bbc184cede90717565a41b68b9
jobailla <jbaillais77@gmail.com>
2021-07-05 15:49:18 +0200

    add extraTasks file

---
+++ extraTasks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# file: extraTasks.sh
+#
+# This script will be launched in background after Witty Pi 2 get initialized.
+# If you want to run your commands after boot, you can place them here.
+#
+cam=$(vcgencmd get_camera | cut -d '=' -f3)
+
+if [ $cam != 0 ]; then
+	sudo python3 /home/pi/RPIZ-timelapse/timelapse.py >> /home/pi/logs/timelapse.log 2>&1
+	sudo rclone copy /home/pi/logs/timelapse.log onedrive:/Timelapse/Pictures/logs
+	sudo rclone copy /home/pi/wittyPi/schedule.log onedrive:/Timelapse/Pictures/logs
+	sudo rclone copy /home/pi/wittyPi/wittyPi.log onedrive:/Timelapse/Pictures/logs
+else
+	echo 'Camera not found'
+fi
