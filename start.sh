bad397d6e53bd93652a7bbd959fa21d16e4f10bf
jobailla <jbaillais77@gmail.com>
2021-07-06 15:02:42 +0200

    rename

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 # This script will be launched in background after Witty Pi 2 get initialized.
 # If you want to run your commands after boot, you can place them here.
 #
+sudo modprobe bcm2835-v4l2
 cam=$(vcgencmd get_camera | cut -d '=' -f3)
 
 if [ $cam != 0 ]; then
@@ -11,7 +12,7 @@ if [ $cam != 0 ]; then
 	sudo rclone copy /home/pi/logs/timelapse.log onedrive:/Timelapse/Pictures/logs
 	sudo rclone copy /home/pi/wittyPi/schedule.log onedrive:/Timelapse/Pictures/logs
 	sudo rclone copy /home/pi/wittyPi/wittyPi.log onedrive:/Timelapse/Pictures/logs
-	gpio -g mode 4 out
+#	gpio -g mode 4 out
 else
 	echo 'Camera not found'
 fi
