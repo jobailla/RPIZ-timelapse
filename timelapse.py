df8f006c01c2519d2135a7b59b00db6bf0bae8b3
jobailla <jbaillais77@gmail.com>
2021-08-04 17:19:49 +0200

    clean

--- timelapse.py
+++ timelapse.py
@@ -1,14 +1,11 @@
 from picamera import PiCamera
-import errno
 import os
 import sys
 import threading
 from datetime import datetime, timedelta
-import time
 from time import sleep
 import yaml
 import subprocess
-import math
 
 config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
 image_number = 0
@@ -21,20 +18,24 @@ cloud_name = (str(config['cloud_name']))
 wittyPath = "/home/pi/wittyPi/"
 timelapsePath = "/home/pi/RPIZ-timelapse/"
 
+
 def getDateTime():
     date = datetime.now().strftime("%Y-%m-%d")
     time = datetime.now().strftime("%H:%M:%S")
     print('*************** ' + date + ' ' + time + ' ***************')
 
+
 def getUpTime():
-    upTime = subprocess.Popen('uptime -s', shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+    upTime = subprocess.Popen('uptime -s', shell=True,
+                              stdout=subprocess.PIPE).stdout.read().decode()
     timeUp = os.popen("awk '{print $1}' /proc/uptime").readline()
     seconds = str(round(float(timeUp)))
-    print('RPI start:\t ', end = ' ')
-    print(upTime, end = '')
-    print('Uptime:\t\t  ' + str(timedelta(seconds = int(seconds))))
+    print('RPI start:\t ', end=' ')
+    print(upTime, end='')
+    print('Uptime:\t\t  ' + str(timedelta(seconds=int(seconds))))
 
-def getSystemInfo(): 
+
+def getSystemInfo():
     GET_THROTTLED_CMD = 'vcgencmd get_throttled'
     GET_CAMERA_INFOS_CMD = 'vcgencmd get_camera'
     GET_TEMPERATURE_CMD = 'vcgencmd measure_temp'
@@ -50,7 +51,8 @@ def getSystemInfo():
         19: 'Soft temperature limit has occurred '
     }
 
-    throttled_output = subprocess.Popen(GET_THROTTLED_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+    throttled_output = subprocess.Popen(
+        GET_THROTTLED_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
     throttled_binary = bin(int(throttled_output.split('=')[1], 0))
 
     warnings = 0
@@ -65,13 +67,16 @@ def getSystemInfo():
     else:
         print("Error: " + message, end='')
 
-    cameraInfo = subprocess.Popen(GET_CAMERA_INFOS_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-    print(' / ', end = '')
-    print('Cam: ' + cameraInfo.strip('\n'), end = '')
-    print(' / ', end = '')
-    
-    temperature = subprocess.Popen(GET_TEMPERATURE_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode().split('=')[1]
-    print(temperature, end = '')
+    cameraInfo = subprocess.Popen(
+        GET_CAMERA_INFOS_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+    print(' / ', end='')
+    print('Cam: ' + cameraInfo.strip('\n'), end='')
+    print(' / ', end='')
+
+    temperature = subprocess.Popen(
+        GET_TEMPERATURE_CMD, shell=True, stdout=subprocess.PIPE).stdout.read().decode().split('=')[1]
+    print(temperature, end='')
+
 
 def calcul_time(date1, date2):
     date1 = date1.split(':')
@@ -83,42 +88,35 @@ def calcul_time(date1, date2):
     diff = date2 - date1
     return diff
 
+
 def str_to_delta(s):
     h, m, s = s.split(':')
     return timedelta(seconds=int(h) * 3600 + int(m) * 60 + int(s))
 
-def delta_to_str(delta):
-    h = delta.seconds//3600
-    m = (delta.seconds-h*3600)//60
-    s = delta.seconds-h*3600-m*60
-    return '{:02d}:{:02d}:{:02d}'.format(h, m, s)
 
 def set_schedule():
     time = datetime.now().time()
     date = datetime.now().date()
-    
+
     time = timedelta(seconds=time.hour * 3600 + time.minute * 60 + time.second)
 
     reboot_interval = config['reboot_interval']
 
     night_start = config['night']['start']
     night_end = config['night']['end']
-    
+
     date_start = config['date']['start']
     date_end = config['date']['end']
-    
+
     start = str_to_delta(config['night']['start'])
     end = str_to_delta(config['night']['end'])
-    
-    night_time = calcul_time(night_end, night_start)
-    night_seconds = night_time.seconds
 
     on_min = config['on_min']
     print('Schedule:\t  ', end='')
 
     if time >= start or time <= end:
         print('NIGHT')
-        date_start =  date + timedelta(days=1)
+        date_start = date + timedelta(days=1)
         begin = str(date_start) + ' ' + str(time)
         end = str(date_end) + ' ' + str(night_end)
         time_off = (str_to_delta(night_end) - time).seconds - on_min
@@ -133,13 +131,15 @@ def set_schedule():
     scheduleFile = open('schedule.wpi', 'w')
     scheduleFile.write('BEGIN' + '  ' + begin + '\n')
     scheduleFile.write('END' + '    ' + end + '\n')
-    scheduleFile.write('ON' + '     ' + 'S' + str(time_on) + ' ' + 'WAIT' + '\n')
+    scheduleFile.write('ON' + '     ' + 'S' +
+                       str(time_on) + ' ' + 'WAIT' + '\n')
     scheduleFile.write('OFF' + '    ' + 'S' + str(time_off) + '\n')
     scheduleFile.close()
 
     os.system("sudo rm -f " + wittyPath + "schedule.wpi")
     os.system("mv " + timelapsePath + "schedule.wpi" + " " + wittyPath)
     subprocess.call("sudo sh " + timelapsePath + "run.sh", shell=True)
+
 
 def set_camera_options(camera):
     # Set camera resolution.
@@ -148,18 +148,15 @@ def set_camera_options(camera):
             config['resolution']['width'],
             config['resolution']['height']
         )
-
     # Set ISO.
     if config['iso']:
         camera.iso = config['iso']
-
     # Set shutter speed.
     if config['shutter_speed']:
         camera.shutter_speed = config['shutter_speed']
         # Sleep to allow the shutter speed to take effect correctly.
         sleep(1)
         camera.exposure_mode = 'off'
-
     # Set white balance.
     if config['white_balance']:
         camera.awb_mode = 'off'
@@ -167,24 +164,29 @@ def set_camera_options(camera):
             config['white_balance']['red_gain'],
             config['white_balance']['blue_gain']
         )
-
     # Set camera rotation
     if config['rotation']:
         camera.rotation = config['rotation']
-
     return camera
+
 
 def add_timestamp():
     for image in image_list:
         t = image.split("-")
-        timestamp = t[2] + '\/' + t[1] + '\/' + t[0] + '\ ' + t[3] + '\:' + t[4] + '\:' + t[5].strip('.jpg')
-        print ("add timestamp: " + image)
-        os.system('convert ' + (dir_path + image + '  -pointsize 42 -fill yellow -annotate +100+100 ' + timestamp + ' ' + str(dir_path) + image))
+        timestamp = t[2] + '\/' + t[1] + '\/' + t[0] + '\ ' + \
+            t[3] + '\:' + t[4] + '\:' + t[5].strip('.jpg')
+        print("add timestamp: " + image)
+        os.system('convert ' + (dir_path + image + '  -pointsize 42 -fill yellow -annotate +100+100 ' +
+                  timestamp + ' ' + str(dir_path) + image))
 
 # Upload picture(s) on cloud (Requires rclone)
+
+
 def sync_cloud():
     print("uploading on " + str(cloud_name) + "...")
-    os.system('sudo rclone copy ' + str(dir_path) + ' ' + str(cloud_name) + ':' + str(cloud_dir))
+    os.system('sudo rclone copy ' + str(dir_path) + ' ' +
+              str(cloud_name) + ':' + str(cloud_dir))
+
 
 def capture_image():
     try:
@@ -194,18 +196,18 @@ def capture_image():
         # picture is taken.
         if (image_number < (config['total_images'] - 1)):
             thread = threading.Timer(config['interval'], capture_image).start()
-        
+
         # Start up the camera.
         camera = PiCamera()
         set_camera_options(camera)
-        
+
         # Capture a picture.
         image_name = datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.jpg'
         camera.capture(str(dir_path) + image_name)
         camera.close()
         image_list.append(image_name)
         print(image_name.replace('.jpg', ''))
-       
+
         if (image_number < (config['total_images'] - 1)):
             image_number += 1
         else:
@@ -217,20 +219,27 @@ def capture_image():
             getDateTime()
             print('===================================================\n')
     except (KeyboardInterrupt):
-        print ("\nTime-lapse capture cancelled.\n")
+        print("\nTime-lapse capture cancelled.\n")
         sys.exit()
     except (SystemExit):
         sys.exit()
 
 # Create an animated gif (Requires ImageMagick)
+
+
 def create_gif():
-    print ('\nCreating animated gif.\n')
-    os.system('convert -delay 10 -loop 0 ' + dir + '/image*.jpg ' + dir + '-timelapse.gif')
+    print('\nCreating animated gif.\n')
+    os.system('convert -delay 10 -loop 0 ' + dir +
+              '/image*.jpg ' + dir + '-timelapse.gif')
 
 # Create a video (Requires avconv)
+
+
 def create_video():
-    print ('\nCreating video.\n')
-    os.system('avconv -framerate 20 -i ' + dir + '/image%08d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')
+    print('\nCreating video.\n')
+    os.system('avconv -framerate 20 -i ' + dir +
+              '/image%08d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')
+
 
 # Main function
 if __name__ == "__main__":
@@ -240,7 +249,8 @@ if __name__ == "__main__":
     getUpTime()
     set_schedule()
     # Kick off the capture process
-    print('Take Picture' + ('s' if config['total_images'] > 1 else '') + ':\t' , end = '')
+    print('Take Picture' +
+          ('s' if config['total_images'] > 1 else '') + ':\t', end='')
     capture_image()
     # Optional actions
     if config['create_gif']:
